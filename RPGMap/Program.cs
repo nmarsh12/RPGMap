4f5f1b250d15a0c5b42763fba8c86d0ba3d90379
nmarsh12 <nathanmarsh6283@gmail.com>
2023-11-06 10:08:19 -0400

    Initial commit

---
+++ RPGMap/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGMap
+{
+    internal class Program
+    {
+        
+        /*
+        Unscaled Map
+        
+         */
+
+        static void Main(string[] args)
+        {
+        }
+
+        
+    }
+
+    class MapRenderer
+    {
+        string[] mapArray;
+        string[] DEFAULT_MAP =
+        {
+            "^^^'''''''''''''''''''''''''''",
+            "^^''''**''''''''''''''''~~~'''",
+            "^^'''**'''''''''''''''~~~'''''",
+            "^'''''''''''''''''''''''''''''",
+            "''''~~~'''''''''''''''''''''''",
+            "''''~~~'''''''''''''''''''''''",
+            "'''~~~~'''''''''''''''^^''''''",
+            "'''''~~~'''''''''''''^^^^'''''",
+            "'''''~~~~''''''''''''''^^^^'''",
+            "'''''''~''''''''''''''''''''''",
+            "''''''''''''''''''''''''''''''",
+            "''''''''''''''''''''''''''''''"
+        };
+        public MapRenderer(string[] _MapStringArray) { 
+            if (_MapStringArray == null)
+            {
+                this.mapArray = DEFAULT_MAP;
+            }
+            else this.mapArray = _MapStringArray;
+        }
+
+        void DisplayMap(int scale=1)
+        {
+            int arrayHorizontalSize = mapArray[0].Length;
+            int arrayVerticalSize = mapArray.Length;
+        }
+    }
+}
